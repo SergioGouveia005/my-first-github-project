d87d4f942482d5a9dd5818f070397dd3ddc9842a
SergioGouveia005 <s5702924@bournemouth.ac.uk>
2024-10-01 17:25:38 +0100

    Added Arithmetic Section

--- my_first_code.py
+++ my_first_code.py
@@ -4,3 +4,15 @@ print("Hello", "World", 2024)
 print("Hello", end=" ")
 print(f"Your name is {name}")
 print("Your name is {}".format(name))
+
+#Arithmetic
+print((10//3)**2)
+for i in range(1, 13):
+    print(f"2 x {i} = {2*i}")
+a = 6
+b = 4
+c = 5
+d = 2
+e = 3
+result = (a*b) + (c**d) - (b%e)
+print(f"({a} * {b}) + ({c} ** {d}) - ({b} % {e}) = {result}")
